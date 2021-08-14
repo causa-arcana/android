9554e2992e31a6d22351fb74f38adb48dcf89a09
Alex Kotov <kotovalexarian@gmail.com>
2021-08-14 06:44:54 +0500

    Display content with WebView

--- app/src/main/java/com/causa_arcana/MainActivity.kt
+++ app/src/main/java/com/causa_arcana/MainActivity.kt
@@ -2,10 +2,31 @@ package com.causa_arcana
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Base64
+import android.webkit.WebView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val mainWebView: WebView = findViewById(R.id.mainWebView)
+
+        val html = """
+            <!DOCTYPE html>
+            <html>
+                <head>
+                    <meta charset="utf-8"/>
+                </head>
+                <body>
+                    <h1>Hello, World!</h1>
+                    <p>This text is rendered with WebView.</p>
+                </body>
+            </html>
+        """.trimIndent()
+
+        val encodedHtml = Base64.encodeToString(html.toByteArray(), Base64.NO_PADDING)
+
+        mainWebView.loadData(encodedHtml, "text/html", "base64")
     }
 }
