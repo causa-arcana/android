3e3f53accd61cb6d3c0dd04cb22c40278a394510
Alex Kotov <kotovalexarian@gmail.com>
2021-08-17 05:28:13 +0500

    Add methods to class ArticleId

--- app/src/main/java/com/causa_arcana/ArticleId.kt
+++ app/src/main/java/com/causa_arcana/ArticleId.kt
@@ -10,17 +10,23 @@ class ArticleId(val year: UInt, val month: UInt, val day: UInt, val slug: String
         if (!SLUG_RE.matches(slug)) throw RuntimeException("Invalid article slug")
     }
 
-    override fun toString(): String {
-        return "${yearPadded()}-${monthPadded()}-${dayPadded()}-$slug"
-    }
+    override fun toString(): String = "${yearPadded()}-${monthPadded()}-${dayPadded()}-$slug"
+    fun toPath():            String = "/${yearPadded()}/${monthPadded()}/${dayPadded()}/$slug"
 
-    private fun yearPadded():  String = year .toString().padStart(4, '0')
-    private fun monthPadded(): String = month.toString().padStart(2, '0')
-    private fun dayPadded():   String = day  .toString().padStart(2, '0')
+    fun toPathWithExt():     String = "${toPath()}.$PATH_EXT"
+    fun toFullPath():        String = "$PATH_PREFIX${toPath()}"
+    fun toFullPathWithExt(): String = "$PATH_PREFIX${toPath()}.$PATH_EXT"
+
+    fun yearPadded():  String = year .toString().padStart(4, '0')
+    fun monthPadded(): String = month.toString().padStart(2, '0')
+    fun dayPadded():   String = day  .toString().padStart(2, '0')
 
     companion object {
         private val ID_RE = """^(\d{4})-(\d\d)-(\d\d)-([a-z0-9]+)(-[a-z0-9]+)*$""".toRegex()
         private val SLUG_RE = """^([a-z0-9]+)(-[a-z0-9]+)*$""".toRegex()
+
+        private const val PATH_PREFIX = "/blog"
+        private const val PATH_EXT = "html"
 
         fun fromString(id: String): ArticleId {
             (ID_RE.matchEntire(id) ?: throw RuntimeException("Invalid article ID")).destructured
--- app/src/test/java/com/causa_arcana/ExampleUnitTest.kt
+++ app/src/test/java/com/causa_arcana/ExampleUnitTest.kt
@@ -5,6 +5,10 @@ import org.junit.Test
 import org.junit.Assert.*
 
 class ExampleUnitTest {
+    private val articleId1 = ArticleId(2021u, 1u, 1u, "hello-world")
+    private val articleId2 = ArticleId(2000u, 6u, 24u, "123")
+    private val articleId3 = ArticleId(3000u, 12u, 6u, "0foo-1-2")
+
     @Test
     fun articleIdFromString() {
         val articleId = ArticleId.fromString("2021-01-01-hello-world")
@@ -16,7 +20,57 @@ class ExampleUnitTest {
 
     @Test
     fun articleIdToString() {
-        val articleId = ArticleId(2021u, 1u, 1u, "hello-world")
-        assertEquals("2021-01-01-hello-world", articleId.toString())
+        assertEquals("2021-01-01-hello-world", articleId1.toString())
+        assertEquals("2000-06-24-123",         articleId2.toString())
+        assertEquals("3000-12-06-0foo-1-2",    articleId3.toString())
+    }
+
+    @Test
+    fun articleIdToPath() {
+        assertEquals("/2021/01/01/hello-world", articleId1.toPath())
+        assertEquals("/2000/06/24/123",         articleId2.toPath())
+        assertEquals("/3000/12/06/0foo-1-2",    articleId3.toPath())
+    }
+
+    @Test
+    fun articleIdToPathWithExt() {
+        assertEquals("/2021/01/01/hello-world.html", articleId1.toPathWithExt())
+        assertEquals("/2000/06/24/123.html",         articleId2.toPathWithExt())
+        assertEquals("/3000/12/06/0foo-1-2.html",    articleId3.toPathWithExt())
+    }
+
+    @Test
+    fun articleIdToFullPath() {
+        assertEquals("/blog/2021/01/01/hello-world", articleId1.toFullPath())
+        assertEquals("/blog/2000/06/24/123",         articleId2.toFullPath())
+        assertEquals("/blog/3000/12/06/0foo-1-2",    articleId3.toFullPath())
+    }
+
+    @Test
+    fun articleIdToFullPathWithExt() {
+        assertEquals("/blog/2021/01/01/hello-world.html", articleId1.toFullPathWithExt())
+        assertEquals("/blog/2000/06/24/123.html",         articleId2.toFullPathWithExt())
+        assertEquals("/blog/3000/12/06/0foo-1-2.html",    articleId3.toFullPathWithExt())
+    }
+
+    @Test
+    fun articleIdYearPadded() {
+        assertEquals("2021", articleId1.yearPadded())
+        assertEquals("2000", articleId2.yearPadded())
+        assertEquals("3000", articleId3.yearPadded())
+    }
+
+    @Test
+    fun articleIdMonthPadded() {
+        assertEquals("01", articleId1.monthPadded())
+        assertEquals("06", articleId2.monthPadded())
+        assertEquals("12", articleId3.monthPadded())
+    }
+
+    @Test
+    fun articleIdDayPadded() {
+        assertEquals("01", articleId1.dayPadded())
+        assertEquals("24", articleId2.dayPadded())
+        assertEquals("06", articleId3.dayPadded())
     }
 }
